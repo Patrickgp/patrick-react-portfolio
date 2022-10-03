874a526efa1417ed9b5af52607b4c5829b7bad0f
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-10-03 12:31:51 -0400

    working on my Projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Projects = () => {
-  return <section></section>;
+  return (
+    <section id="projects">
+      <div className="container">
+        <div className="my-5 d-flex justify-content-center">
+          <h1>My Past Projects</h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
