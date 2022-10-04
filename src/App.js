d20f77f7bfbb44bd0ab59ce7150e6b561d3e7e7d
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-10-04 13:29:13 -0400

    restarted hooray

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import Nav from "./components/Nav";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact";
 
 function App() {
-  return (
-    <main>
-      <Nav />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-    </main>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
