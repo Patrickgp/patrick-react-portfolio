b9c1f7e9ec538d611df68eb46d08818343a1ad6d
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-10-06 09:00:18 -0400

    added about component

--- src/components/Skill/index.js
+++ src/components/Skill/index.js
@@ -8,6 +8,19 @@ const Skill = () => {
           <h2 className="project-header my-5 d-flex justify-content-center">
             My Skills
           </h2>
+          <div className="d-flex justify-content-center">
+            <ul className="skillsList">
+              <li>HTML, CSS, JS</li>
+              <li>React.js</li>
+              <li>Bootstrap, Materialize, Tailwind</li>
+              <li>jQuery</li>
+              <li>Express.js</li>
+              <li>Node.js</li>
+              <li>MySQL & Sequelize ORM</li>
+              <li>MongoDB & Mongoose ODM</li>
+              <li>restAPI</li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
