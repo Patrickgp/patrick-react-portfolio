2d6e6d88d5b9dbee83559b8384a7d2b32133e68d
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-10-09 09:36:58 -0400

    handling different route paths with gh pages deployment

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -15,6 +15,7 @@ function Main() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/patrick-react-portfolio" element={<About />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/skill" element={<Skill />} />
